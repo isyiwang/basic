52554a90fcd55493d3338b2f56ff0116df7210bb
fangbrian <brian@edtwist.com>
2015-06-27 16:50:11 -0700

    Implement generating photosets with 5 second timeout

--- Basic/app/src/main/java/com/ggl/android/basic/PhotoSetManager.java
+++ Basic/app/src/main/java/com/ggl/android/basic/PhotoSetManager.java
@@ -1,15 +1,44 @@
 package com.ggl.android.basic;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by isaac on 6/27/15.
  */
 public class PhotoSetManager implements GalleryChangeManager.IOnGalleryChangedListener {
+    public static class PhotoSet extends ArrayList<Photo> {}
     public static String TAG = PhotoSetManager.class.getSimpleName();
+
+    private static final int PHOTOSET_POST_DELAY_MS = 5000;
+    private PhotoSet photoSet = new PhotoSet();
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private Runnable runnable;
 
     @Override
     public void onPhotoAdded(Photo photo) {
         Log.d(TAG, "Photo received: " + photo);
+        if (runnable != null) handler.removeCallbacks(runnable);
+
+        photoSet.add(photo);
+        runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                // Notify listenerr 
+                Log.d(TAG, "Posting runnable with " + photoSet.size() + " photos");
+                photoSet.clear();
+
+            }
+        };
+
+        handler.postDelayed(runnable, PHOTOSET_POST_DELAY_MS);
     }
+
+
 }
