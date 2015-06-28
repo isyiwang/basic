ba9d4e60fd3d92eed43f6265c74a911d36bf00a5
isyiwang <isaacwang@gmail.com>
2015-06-27 19:01:29 -0700

    Add ViewPager to MainActivity, load with Picasso, integrate new ImageFragment - basically the meat

--- Basic/app/src/main/java/com/ggl/android/basic/MainActivity.java
+++ Basic/app/src/main/java/com/ggl/android/basic/MainActivity.java
@@ -1,10 +1,25 @@
 package com.ggl.android.basic;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,23 +27,27 @@ public class MainActivity extends ActionBarActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     public static final String PHOTOSET_TIMESTAMP_EXTRA = "PHOTOSET_TIMESTAMP_EXTRA";
+    private ViewPager mViewPager;
+    private PhotoAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mViewPager = (ViewPager)findViewById(R.id.view_pager);
 
         displayPhotoSetFromIntent();
     }
 
     private void displayPhotoSetFromIntent() {
-        if (getIntent() == null) return;
+        if (getIntent() == null || getIntent().getExtras() == null) return;
 
         Long timestamp = getIntent().getExtras().getLong(PHOTOSET_TIMESTAMP_EXTRA, 0);
         PhotoSetManager.PhotoSet photoSet = PhotoSetManager.getInstance().getPhotoSetForTimestamp(timestamp);
         if (photoSet == null) return;
 
-        Log.d(TAG, "Displaying photo set with " + photoSet.size() + " photos");
+        mAdapter = new PhotoAdapter(getSupportFragmentManager(), photoSet);
+        mViewPager.setAdapter(mAdapter);
     }
 
     @Override
@@ -51,5 +70,61 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class PhotoAdapter extends FragmentPagerAdapter {
+        private final PhotoSetManager.PhotoSet mPhotoSet;
+
+        public PhotoAdapter(FragmentManager fm, PhotoSetManager.PhotoSet photoSet) {
+            super(fm);
+
+            mPhotoSet = photoSet;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Photo photo = mPhotoSet.get(position);
+            return ImageFragment.newInstance(photo.getPath());
+        }
+
+        @Override
+        public int getCount() {
+            return mPhotoSet.size();
+        }
+    }
+
+    public static class ImageFragment extends Fragment {
+        public static final String PATH_BUNDLE_KEY = "PATH_BUNDLE_KEY";
+        private String mPath;
+
+        static ImageFragment newInstance(String path) {
+            ImageFragment f = new ImageFragment();
+
+            // Supply num input as an argument.
+            Bundle args = new Bundle();
+            args.putString(PATH_BUNDLE_KEY, path);
+            f.setArguments(args);
+
+            return f;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            Bundle bundle = getArguments();
+            mPath = (bundle == null) ? "" : bundle.getString(PATH_BUNDLE_KEY);
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_image, container, false);
+            ImageView imageView = (ImageView) v.findViewById(R.id.image_view);
+            File imgFile = new File(mPath);
+            Picasso.with(getActivity()).load(imgFile).fit().centerInside().into(imageView);
+
+            return v;
+        }
     }
 }
